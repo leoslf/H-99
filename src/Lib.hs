efab446ce62f84988345a04efe0946e3eec32add
Leo Sin <leo.sin@my.cityu.edu.hk>
2022-08-15 02:42:38 +0800

    added Problem 11-20

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,40 +1,40 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Lib where
 
--- | Problem 1. Find the last element of a list.
+-- | Problem 1: Find the last element of a list.
 myLast :: [a] -> a
 myLast [] = error "empty list"
 myLast (x:[]) = x
 myLast (_:xs) = myLast xs
 
--- | Problem 2. Find the last but one element of a list.
+-- | Problem 2: Find the last but one element of a list.
 myButLast :: [a] -> a
 myButLast [] = error "empty list"
 myButLast (_:[]) = error "list with one element"
 myButLast (x:_:[])  = x
 myButLast (_:xs) = myButLast xs
 
--- | Problem 3. Find the K'th element of a list. The first element in the list is number 1.
+-- | Problem 3: Find the K'th element of a list. The first element in the list is number 1.
 elementAt :: Integral d => [a] -> d -> a
 elementAt [] _ = error "empty list"
 elementAt (x:_) 1 = x
 elementAt (x:xs) index = elementAt xs (index - 1)
 
--- | Problem 4. Find the number of elements of a list.
+-- | Problem 4: Find the number of elements of a list.
 myLength :: Integral d => [a] -> d
 myLength [] = 0
 myLength (x:xs) = 1 + myLength xs
 
--- | Problem 5. Reverse a list.
+-- | Problem 5: Reverse a list.
 myReverse :: [a] -> [a]
 myReverse [] = []
 myReverse (x:xs) = myReverse xs ++ [x]
 
--- | Problem 6. Find out whether a list is a palindrome. A palindrome can be read forward or backward; e.g. (x a m a x).
+-- | Problem 6: Find out whether a list is a palindrome. A palindrome can be read forward or backward; e.g. (x a m a x).
 isPalindrome :: Eq a => [a] -> Bool
 isPalindrome xs = xs == reverse xs 
 
--- | Problem 7. Flatten a nested list structure.
+-- | Problem 7: Flatten a nested list structure.
 -- Transform a list, possibly holding lists as elements into a `flat' list by replacing each list with its elements (recursively).
 data NestedList a = Elem a | List [NestedList a]
 flatten :: NestedList a -> [a]
@@ -42,7 +42,7 @@ flatten (Elem x) = [x]
 flatten (List xs) = concat $ map flatten xs
   
 
--- | Problem 8. Eliminate consecutive duplicates of list elements.
+-- | Problem 8: Eliminate consecutive duplicates of list elements.
 compress :: Eq a => [a] -> [a]
 compress [] = []
 compress [x] = [x]
@@ -51,7 +51,7 @@ compress (x:x':xs)
   | otherwise = x:(compress $ x':xs)
 -- compress = map head . group
 
--- | Problem 9. Pack consecutive duplicates of list elements into sublists. If a list contains repeated elements they should be placed in separate sublists.
+-- | Problem 9: Pack consecutive duplicates of list elements into sublists. If a list contains repeated elements they should be placed in separate sublists.
 pack :: Eq a => [a] -> [[a]]
 pack [] = []
 pack [x] = [[x]]
@@ -61,6 +61,81 @@ pack (x:xs)
   where
     group:groups = pack xs
 
--- | Probelm 10. Run-length encoding of a list. Use the result of problem P09 to implement the so-called run-length encoding data compression method. Consecutive duplicates of elements are encoded as lists (N E) where N is the number of duplicates of the element E.
+-- | Problem 10: Run-length encoding of a list. Use the result of problem P09 to implement the so-called run-length encoding data compression method. Consecutive duplicates of elements are encoded as lists (N E) where N is the number of duplicates of the element E.
 encode :: Eq a => [a] -> [(Int, a)]
 encode xs = map (\group -> (length group, head group)) $ pack xs
+
+-- | Problem 11: Modified run-length encoding.
+-- Modify the result of problem 10 in such a way that if an element has no duplicates it is simply copied into the result list. Only elements with duplicates are transferred as (N E) lists.
+data Eq a => ModifiedEncoding a
+  = Single a
+  | Multiple Int a
+  deriving (Show, Eq)
+
+modifiedEncoding :: Eq a => (Int, a) -> ModifiedEncoding a
+modifiedEncoding (1, x) = Single x
+modifiedEncoding (n, x) = Multiple n x
+
+encodeModified :: Eq a => [a] -> [ModifiedEncoding a]
+encodeModified xs = map modifiedEncoding $ encode xs
+
+-- | Problem 12: Decode a run-length encoded list.
+-- Given a run-length code list generated as specified in problem 11. Construct its uncompressed version.
+decodeModified :: Eq a => [ModifiedEncoding a] -> [a]
+decodeModified encoded = concat $ map decode encoded
+  where
+    decode (Single x) = [x]
+    decode (Multiple n x) = replicate n x
+
+-- | Problem 13: Run-length encoding of a list (direct solution).
+-- Implement the so-called run-length encoding data compression method directly. I.e. don't explicitly create the sublists containing the duplicates, as in problem 9, but only count them. As in problem P11, simplify the result list by replacing the singleton lists (1 X) by X.
+encodeDirect :: Eq a => [a] -> [ModifiedEncoding a]
+encodeDirect = map modifiedEncoding . encode'
+  where
+    encode' = foldr f []
+    f x [] = [(1, x)]
+    f x ((n, y):ys)
+      | x == y = (n + 1, x):ys
+      | otherwise = (1, x):(n, y):ys 
+
+-- | Problem 14: Duplicate the elements of a list.
+dupli :: [a] -> [a]
+dupli [] = []
+dupli (x:xs) = x:x:dupli xs
+
+-- | Problem 15: Replicate the elements of a list a given number of times.
+repli :: [a] -> Int -> [a]
+repli [] _ = []
+repli (x:xs) n = (take n $ repeat x) ++ (repli xs n)
+
+-- | Problem 16: Drop every N'th element from a lis.
+dropEvery xs n = unenumerate $ filter (\(i, _) -> i `mod` n /= (n - 1)) $ enumerate xs
+  where
+    enumerate = zip [0..]
+    unenumerate = map snd
+
+-- | Problem 17: Split a list into two parts; the length of the first part is given.
+
+-- Do not use any predefined predicates.
+split :: [a] -> Int -> ([a], [a])
+split xs n = helper ([], xs) n
+  where
+    helper (xs, ys) 0 = (xs, ys)
+    helper (xs, []) n = (xs, [])
+    helper (xs, y:ys) n = helper (xs ++ [y], ys) (n - 1)
+
+-- | Problem 18: Extract a slice from a list.
+-- Given two indices, i and k, the slice is the list containing the elements between the i'th and k'th element of the original list (both limits included). Start counting the elements with 1.
+slice :: [a] -> Int -> Int -> [a]
+slice xs lo hi = drop (lo - 1) $ take hi xs
+
+-- | Problem 19: Rotate a list N places to the left.
+-- Hint: Use the predefined functions length and (++).
+rotate :: [a] -> Int -> [a]
+rotate xs n = drop n' xs ++ take n' xs
+  where
+    n' = (length xs + n) `mod` (length xs)
+
+-- | Problem 20: Remove the K'th element from a list.
+removeAt :: Int -> [a] -> (a, [a])
+removeAt k xs = (xs !! (k - 1), take (k - 1) xs ++ drop k xs)
--- test/Spec.hs
+++ test/Spec.hs
@@ -6,12 +6,14 @@ import Lib
 
 main :: IO ()
 main = hspec $ do
+  -- Problem 1
   describe "myLast" $ do
     it "returns the last element of the list" $ do
       myLast [1, 2, 3, 4] `shouldBe` 4
       myLast ['x', 'y', 'z'] `shouldBe` 'z'
       evaluate (myLast []) `shouldThrow` errorCall "empty list"
 
+  -- Problem 2
   describe "myButLast" $ do
     it "returns the last but one element of a list" $ do
       myButLast [1, 2, 3, 4] `shouldBe` 3
@@ -19,40 +21,103 @@ main = hspec $ do
       evaluate (myButLast []) `shouldThrow` errorCall "empty list"
       evaluate (myButLast [1]) `shouldThrow` errorCall "list with one element"
 
+  -- Problem 3
   describe "elementAt" $ do
     it "returns the K'th element of a list, 1-indexed" $ do
       elementAt [1,2,3] 2 `shouldBe` 2
       elementAt "haskell" 5 `shouldBe` 'e'
 
+  -- Problem 4
   describe "myLength" $ do
     it "returns the number of elements of a list" $ do
       myLength [123, 456, 789] `shouldBe` 3
       myLength "Hello, world!" `shouldBe` 13
       myLength [] `shouldBe` 0
 
+  -- Problem 5
   describe "myReverse" $ do
     it "returns the reversed copy of the list" $ do
       myReverse [] `shouldBe` ([] :: [Int])
       myReverse "A man, a plan, a canal, panama!" `shouldBe` "!amanap ,lanac a ,nalp a ,nam A"
       myReverse [1,2,3,4] `shouldBe` [4,3,2,1]
 
+  -- Problem 6
   describe "isPalindrome" $ do
     it "returns whether a list is a palindrome" $ do
       isPalindrome [1,2,3] `shouldBe` False
       isPalindrome "madamimadam" `shouldBe` True
       isPalindrome [1,2,4,8,16,8,4,2,1] `shouldBe` True
 
+  -- Problem 7
   describe "flatten" $ do
     it "flattens a nested list structure" $ do
       flatten (Elem 5) `shouldBe` [5]
       flatten (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]]) `shouldBe` [1,2,3,4,5]
       flatten (List []) `shouldBe` ([] :: [Int])
 
+  -- Problem 8
   describe "compress" $ do
     it "eliminates consecutive duplicates of list elements." $ do
       compress "aaaabccaadeeee" `shouldBe` "abcade"
       compress [] `shouldBe` ([] :: [Int])
 
+  -- Problem 9
+  describe "pack" $ do
+    it "packs consecutive duplicates of list elements into sublists" $ do
+      pack "aaaabccaadeeee" `shouldBe` ["aaaa","b","cc","aa","d","eeee"]
+
+  -- Problem 10
   describe "encode" $ do
     it "returns the run-length encoding of a list" $ do
       encode "aaaabccaadeeee" `shouldBe` [(4,'a'),(1,'b'),(2,'c'),(2,'a'),(1,'d'),(4,'e')]
+
+  -- Problem 11
+  describe "encodeModified" $ do
+    it "packages the encoding into custom datatype" $ do
+      encodeModified "aaaabccaadeeee" `shouldBe` [Multiple 4 'a',Single 'b',Multiple 2 'c', Multiple 2 'a',Single 'd',Multiple 4 'e']
+
+  -- Problem 12
+  describe "decodeModified" $ do
+    it "decodes the custom datatype of the run-length encoding" $ do
+      decodeModified [Multiple 4 'a',Single 'b',Multiple 2 'c',Multiple 2 'a',Single 'd',Multiple 4 'e'] `shouldBe` "aaaabccaadeeee"
+
+  -- Problem 13
+  describe "encodeDirect" $ do
+    it "run-length encodes a list directly" $ do
+      encodeDirect "aaaabccaadeeee" `shouldBe` [Multiple 4 'a',Single 'b',Multiple 2 'c',Multiple 2 'a',Single 'd',Multiple 4 'e']
+
+  -- Problem 14
+  describe "dupli" $ do
+    it "duplicate the elements of a list" $ do
+      dupli [1, 2, 3] `shouldBe` [1,1,2,2,3,3]
+
+  -- Problem 15
+  describe "repli" $ do
+    it "replicates the elements of a list a given number of times" $ do
+       repli "abc" 3 `shouldBe` "aaabbbccc"
+
+  -- Problem 16
+  describe "dropEvery" $ do
+    it "drops every n-th element from a list" $ do
+      dropEvery "abcdefghik" 3 `shouldBe` "abdeghk"
+  
+  -- Problem 17
+  describe "split" $ do
+    it "splits a list into two parts; the length of the first part is given." $ do
+      split "abcdefghik" 3 `shouldBe` ("abc", "defghik")
+
+  -- Problem 18
+  describe "slice" $ do
+    it "extracts a slice from a list" $ do
+      slice "abcdefghik" 3 7 `shouldBe` "cdefg"
+
+  -- Problem 19
+  describe "rotate" $ do
+    it "rotates a list n places to the left" $ do
+      rotate "abcdefgh" 3 `shouldBe` "defghabc"
+      rotate "abcdefgh" (-2) `shouldBe` "ghabcdef"
+
+  -- Problem 20
+  describe "removeAt" $ do
+    it "removes the k-th element from a list" $ do
+      removeAt 2 "abcd" `shouldBe` ('b',"acd")
